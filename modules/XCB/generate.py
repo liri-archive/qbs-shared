37dafaf63430087d9fb9d2ab6cb9f46dddc0429a
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2017-05-08 23:46:02 +0200

    Add XCB dependency

---
+++ modules/XCB/generate.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import os
+
+template = """import qbs 1.0
+
+Module {
+    property bool found: XCB.xcb.found && probe.found
+
+    Depends { name: "cpp" }
+    Depends { name: "XCB.xcb" }
+
+    cpp.defines: base.concat(probe.defines == undefined ? [] : probe.defines)
+    cpp.commonCompilerFlags: base.concat(probe.compilerFlags == undefined ? [] : probe.compilerFlags)
+    cpp.includePaths: base.concat(probe.includePaths == undefined ? [] : probe.includePaths)
+    cpp.libraryPaths: base.concat(probe.libraryPaths == undefined ? [] : probe.libraryPaths)
+    cpp.dynamicLibraries: base.concat(probe.libraries == undefined ? [] : probe.libraries)
+    cpp.linkerFlags: base.concat(probe.linkerFlags == undefined ? [] : probe.linkerFlags)
+
+    LiriPkgConfigProbe {
+        id: probe
+        name: "%s"
+    }
+}
+"""
+
+modules = [
+    'xcb-atom',
+    'xcb-cursor',
+    'xcb-dri2',
+    'xcb-ewmh',
+    'xcb-image',
+    'xcb-present',
+    'xcb-record',
+    'xcb-res',
+    'xcb-shm',
+    'xcb-xf86dri',
+    'xcb-xinput',
+    'xcb-xvmc',
+    'xcb-aux',
+    'xcb-damage',
+    'xcb-dri3',
+    'xcb-glx',
+    'xcb-keysyms',
+    'xcb-proto',
+    'xcb-render',
+    'xcb-screensaver',
+    'xcb-sync',
+    'xcb-xfixes',
+    'xcb-xkb',
+    'xcb-xv',
+    'xcb-composite',
+    'xcb-dpms',
+    'xcb-event',
+    'xcb-icccm',
+    'xcb-randr',
+    'xcb-renderutil',
+    'xcb-shape',
+    'xcb-util',
+    'xcb-xinerama',
+    'xcb-xtest',
+]
+
+for module in modules:
+    name = module[4:]
+    if not os.path.exists(name):
+        os.makedirs(name)
+    with open(os.path.join(name, name + ".qbs"), "w") as f:
+        f.write(template % module)
---
+++ modules/XCB/xkb/xkb.qbs
@@ -0,0 +1,20 @@
+import qbs 1.0
+
+Module {
+    property bool found: XCB.xcb.found && probe.found
+
+    Depends { name: "cpp" }
+    Depends { name: "XCB.xcb" }
+
+    cpp.defines: base.concat(probe.defines == undefined ? [] : probe.defines)
+    cpp.commonCompilerFlags: base.concat(probe.compilerFlags == undefined ? [] : probe.compilerFlags)
+    cpp.includePaths: base.concat(probe.includePaths == undefined ? [] : probe.includePaths)
+    cpp.libraryPaths: base.concat(probe.libraryPaths == undefined ? [] : probe.libraryPaths)
+    cpp.dynamicLibraries: base.concat(probe.libraries == undefined ? [] : probe.libraries)
+    cpp.linkerFlags: base.concat(probe.linkerFlags == undefined ? [] : probe.linkerFlags)
+
+    LiriPkgConfigProbe {
+        id: probe
+        name: "xcb-xkb"
+    }
+}

0805dda2889364b11cb95c4b241f0227655dd6dc
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2017-05-14 23:24:10 +0200

    Add Qt5GStreamer dependency module

---
+++ modules/Qt5GStreamer/generate.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+
+template = """import qbs 1.0
+
+Module {
+    property bool found: probe.found
+
+    Depends { name: "cpp" }
+
+    cpp.defines: base.concat(probe.defines == undefined ? [] : probe.defines)
+    cpp.commonCompilerFlags: base.concat(probe.compilerFlags == undefined ? [] : probe.compilerFlags)
+    cpp.includePaths: base.concat(probe.includePaths == undefined ? [] : probe.includePaths)
+    cpp.libraryPaths: base.concat(probe.libraryPaths == undefined ? [] : probe.libraryPaths)
+    cpp.dynamicLibraries: base.concat(probe.libraries == undefined ? [] : probe.libraries)
+    cpp.linkerFlags: base.concat(probe.linkerFlags == undefined ? [] : probe.linkerFlags)
+
+    LiriPkgConfigProbe {
+        id: probe
+        name: "%s"
+    }
+}
+"""
+
+modules = [
+    'Qt5GLib-2.0',
+    'Qt5GStreamerQuick-1.0',
+    'Qt5GStreamerUi-1.0',
+    'Qt5GStreamerUtils-1.0',
+]
+
+for module in modules:
+    name = module[3:-4]
+    if name.startswith('GStreamer'):
+        name = name[9:]
+    if not os.path.exists(name):
+        os.makedirs(name)
+    with open(os.path.join(name, name + ".qbs"), "w") as f:
+        f.write(template % module)

5f3ddb545122b2a39d7e943d8a842b2326fc71aa
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-04-05 02:15:51 +0200

    Update infra-travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,27 +2,19 @@
 
 set -e
 
-msg() {
-    lightblue='\033[1;34m'
-    reset='\e[0m'
-    echo -e "${lightblue}$@${reset}"
-}
-
-# Install
-msg "Install packages..."
-apt-get install -y \
-    g++ clang \
-    git \
-    qbs \
-    qt5-default
+source /usr/local/share/liri-travis/functions
 
 # Configure qbs
+travis_start "qbs_setup"
 msg "Setup qbs..."
 qbs setup-toolchains --detect
 qbs setup-qt $(which qmake) travis-qt5
 qbs config profiles.travis-qt5.baseProfile $CC
+travis_end "qbs_setup"
 
 # Build
+travis_start "build"
 msg "Build..."
 qbs -d build -j $(nproc) --all-products profile:travis-qt5 \
     project.prefix:/usr
+travis_end "build"
